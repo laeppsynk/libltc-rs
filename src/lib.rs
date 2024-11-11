112984e000a2fd5151efac88bf874e425fa8a55c
druskus20 <dev@druskus.com>
2024-11-11 19:15:23 +0100

    fix: narrow down allow statements

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,4 @@
 // lib.rs
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
 
 mod api;
 mod error;
--- src/raw.rs
+++ src/raw.rs
@@ -1,3 +1,7 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
 pub(crate) use autogen::*;
 
 // Several functions in this library are deprecated. We allow dead code to avoid warnings.
